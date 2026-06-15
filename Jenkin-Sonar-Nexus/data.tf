a9f5485bf517b24f82c952e466c9a1f3a220ee70
Shreyas Sangai <sangaishreyas8@gmail.com>
2026-06-15 17:36:16 +0530

    Folder Upload

--- Jenkin-Sonar-Nexus/data.tf
+++
@@ -1,4 +0,0 @@
-data "aws_availability_zones" "available" {  # "aws_availability_zones" helps to fetch data from the aws about az
-  state = "available" # only available az are selected
-
-}
--- Jenkin-Sonar-Nexus/provider.tf
+++
@@ -1,3 +0,0 @@
-provider "aws" { # we are using the aws provider
-  region = "ap-south-1" # we are using the ap-south-1 region
-}
